96175d2e4253f3baf2ac5b3a8c2f17faad1cf612
h0tl33t <h0tl33t@gmail.com>
2016-09-24 15:16:47 -0400

    Allow leaders to manage user role & active status

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,15 +2,22 @@ ActiveAdmin.register User do
   permit_params :name,
                 :email,
                 :password,
-                :password_confirmation
+                :password_confirmation,
+                :role,
+                :active
 
   filter :name_or_email_cont, label: 'Search'
 
+  scope :active, default: true
+  scope :leader
+  scope :volunteer
+  scope :inactive
+
   index do
+    column :name
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column :role
+    column :active
 
     actions
   end
@@ -21,15 +28,43 @@ ActiveAdmin.register User do
       input :email
       input :password
       input :password_confirmation
+
+      if current_user.leader?
+        input :role, as: :select, collection: User::ROLES
+        input :active
+      end
     end
 
     actions
   end
 
   controller do
+    before_action :strip_empty_password_parameters, only: :update
+    before_action :sanitize_leader_only_attributes, only: [:create, :update]
+
     def show
       show! do |success, failure|
         success.html { redirect_to [:edit, resource] }
+      end
+    end
+
+    private
+
+    def find_resource
+      User.find_by!(slug: params[:id])
+    end
+
+    def strip_empty_password_parameters
+      if params[:user][:password].empty?
+        params[:user].delete(:password)
+        params[:user].delete(:password_confirmation)
+      end
+    end
+
+    def sanitize_leader_only_attributes
+      if current_user.volunteer?
+        params[:user].delete(:role)
+        params[:user].delete(:active)
       end
     end
   end
